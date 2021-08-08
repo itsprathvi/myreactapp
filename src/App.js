5a4a8a94cb9f1057a81bd6ccef6f568c0089256c
itsprathvi <prathvirajbellampalli@gmail.com>
2021-08-08 22:25:31 +0530

    React Component 1

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
+import React, { Component } from 'react';
 import logo from './logo.svg';
-import { Navbar } from 'reactstrap';
+import { Navbar, NavbarBrand } from 'reactstrap';
+import Menu from './components/menuComponent';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar dark color="primary">
-        <div className="container">
-          <navbarBrand href="/" >Ristorante Con Fusion</navbarBrand>
-        </div>
-      </Navbar>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/" >Ristorante Con Fusion</NavbarBrand>
+          </div>
+        </Navbar>
+        <Menu />
+      </div>
+    );
+  }
 }
 
 export default App;
